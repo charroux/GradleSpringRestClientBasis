3da59bb3b2a96cb667c4d44ac1bfe1085a4aeb49
charroux <benoit.charroux@free.fr>
2015-03-02 18:46:01 +0100

    first commit

---
+++ src/main/java/dto/CarDTO.java
@@ -0,0 +1,22 @@
+package dto;
+
+public class CarDTO {
+	
+	String plateNumber;
+
+	public String getPlateNumber() {
+		return plateNumber;
+	}
+
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "CarDTO [plateNumber=" + plateNumber + "]";
+	}
+	
+	
+
+}
